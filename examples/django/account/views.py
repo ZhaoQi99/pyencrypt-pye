c1837c06a2c12ce91c61fd80baa747419849075e
Qi Zhao <zhaoqi99@outlook.com>
2025-07-23 16:41:59 +0800

    refactor: 💡 django license examples

--- examples/django/account/views.py
+++ examples/django/account/views.py
@@ -6,6 +6,17 @@ class LoginView(View):
     def get(self, request, *args, **kwargs):
         username = request.GET["username"]
         password = request.GET["password"]
+        try:
+            import loader
+
+            file_loader = loader.EncryptFileLoader("")
+            if file_loader.license is True and file_loader.check() is False:
+                return JsonResponse({"message": "License is not valid"}, status=403)
+        except ModuleNotFoundError:
+            pass
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status=403)
+
         if username == "admin" and password == "admin":
             return JsonResponse(
                 {
